0936dd0dadaf3cb839263ea1d52f98005f599bd6
treeps <treeps@student.42wolfsburg.de>
2023-03-13 18:10:44 +0100

    trying stuff

--- src/sierpinski.c
+++ src/sierpinski.c
@@ -6,36 +6,77 @@
 /*   By: treeps <treeps@student.42wolfsbur>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:07:38 by treeps            #+#    #+#             */
+/*   Updated: 2023/03/13 16:42:24 by treeps           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sierpinski.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: treeps <treeps@student.42wolfsbur>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/13 15:07:38 by treeps            #+#    #+#             */
 /*   Updated: 2023/03/13 15:07:51 by treeps           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fractol.h"
 
+static void	translate(double x, double y, int *x_cord, int *y_cord)
+{
+	*x_cord = linear_scale(x, 1, 10, WIDTH - 10);
+	*y_cord = linear_scale(y, 1, 10, HEIGHT - 10);
+}
+
 void	init_sierpinski(t_data *data)
 {
-	data->max_iteration = 1;
-	data->color_set = 420;
-	data->lum_min = -3;
-	data->lum_max = 1.5;
-	data->lum = data->lum_min;
-	data->lum_offset = 0.01;
+	int	x_cord;
+	int	y_cord;
+
+	data->max_iteration = 10000;
+	data->fractal = 2;
+	data->pos[0] = 0.433;
+	data->pos[1] = 0.5;
+	data->abc[0][0] = 1.0;
+	data->abc[0][1] = 1.0;
+	data->abc[1][0] = 0.0;
+	data->abc[1][1] = 1.0;
+	data->abc[2][0] = 0.5;
+	data->abc[2][1] = 0.0;
+	translate(data->abc[0][0], data->abc[0][1], &x_cord, &y_cord);
+	my_mlx_pixel_put(data, x_cord, y_cord, 0xFF0000);
+	translate(data->abc[1][0], data->abc[1][1], &x_cord, &y_cord);
+	my_mlx_pixel_put(data, x_cord, y_cord, 0x00FF00);
+	translate(data->abc[2][0], data->abc[2][1], &x_cord, &y_cord);
+	my_mlx_pixel_put(data, x_cord, y_cord, 0x0000FF);
+	translate(data->pos[0], data->pos[1], &x_cord, &y_cord);
+	my_mlx_pixel_put(data, x_cord, y_cord, 0xFFFFFF);
 }
 
-static void	calc(t_data *data)
+static int	calc(t_data *data, int x, int y)
 {
-	static int	arr[1000][1000];
-	int			corner_abc[3][2];
+	int	corner;
+	int	x_cord;
+	int	y_cord;
+	int	i;
 
-	corner_abc[0][0] = 0;
-	corner_abc[0][1] = 0;
-	corner_abc[1][0] = 1000;
-	corner_abc[1][1] = 0;
-	corner_abc[2][0] = 500;
-	corner_abc[2][1] = 1000;
+	i = 0;
+	while (i < data->max_iteration)
+	{
+		corner = rand() % 3;
+		data->pos[0] += (data->abc[corner][0] - data->pos[0]) * 0.5;
+		data->pos[1] += (data->abc[corner][1] - data->pos[1]) * 0.5;
+		translate(data->pos[0], data->pos[1], &x_cord, &y_cord);
+		i++;
+		if (x_cord == x && y_cord == y)
+			return (i);
+	}
+	return (i);
 }
 
-static void	put_picture(t_data *data)
+static void	put(t_data *data)
 {
 	int	x;
 	int	y;
@@ -48,7 +89,7 @@ static void	put_picture(t_data *data)
 		y = 0;
 		while (y < HEIGHT)
 		{
-			calc(data);
+			iteration = calc(data, x, y);
 			color = get_color(data, iteration);
 			my_mlx_pixel_put(data, x, y, color);
 			y++;
@@ -59,8 +100,8 @@ static void	put_picture(t_data *data)
 
 int	put_sierpinski(t_data *data)
 {
-	put_picture(data);
+	// calc(data);
+	put(data);
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->img, 0, 0);
-	data->max_iteration++;
 	return (0);
 }
